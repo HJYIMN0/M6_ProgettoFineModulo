ac2f1e5957f78cdd97e9e25863658d644a384579
HJYIMN0 <sim0allaseconda@gmail.com>
2025-09-21 20:08:52 +0200

    Other models imported

--- Assets/ReadMe.cs
+++ Assets/ReadMe.cs
@@ -4,16 +4,18 @@
 //Sistema di monete
 //playermovement
 //PlayerParticlesSystem
+//Implementing Characters and Environment
+//Skybox (Added to my assets, i need to import it from packageManager)
 
 //TO DO:
-//MAGARI POTER FARE SALTARE DA ALCUNE PARETI
-//AGGIUNGERE IL SISTEMA DI SALVATAGGIO CHE HO FATTO PER LA BUILD WEEK
 //AGGIUNGERE UN GAME MANAGER CHE GESTISCA IL LIVELLO, L'AUDIO E ALTRO
 //Finire la logica di winning trigger aggiungendo il raccoglitore di monete
 //Completare il sistema di salvataggio con il vector 3
 //Aggiustare il sistema di raccolta monete con un CoinManager
 //Ricontrollare sistema sconfitta e vittoria, ho visto che c'era un problema col prefab e non ho riprovato
-//I.e. Aggiungi una linea che con input.getbutton fa comparire il fade in   
+//I.e. Aggiungi una linea che con input.getbutton fa comparire il fade in
+//Animazioni dell'enemy
+//Correzione bullet logic
 
 
 // Ho aggiunto i seguenti script:
@@ -28,12 +30,15 @@
 //PlayerJumpController
 //PlayerParticleSystem
 
+
 //Ho modificato i seguenti script:
 //TimeManager, rendendolo un singleton
 //CoinSystem, aggiungendo il CoinManager
 //Tolto Lo script dalla camera, sostituendolo con CineMachine
 //PlayerController, Separandolo in 3 differenti scirpts
+//PlayerAnimatorController, correggendolo e migliorandolo
 
 //Cosa rivedere in base alle modifiche:
 //Gli script di UI
+//Gli script delle trappole
 
