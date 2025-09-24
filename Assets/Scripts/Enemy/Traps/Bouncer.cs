f1373d90130e30f8ce71cb9e8a2eeea919603775
HJYIMN0 <sim0allaseconda@gmail.com>
2025-09-24 18:11:06 +0200

    Traps logic implemented

--- Assets/Scripts/Enemy/Traps/Bouncer.cs
+++ Assets/Scripts/Enemy/Traps/Bouncer.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class Bouncer : AbstractEnemy, iMovable
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _pushStrength = 2f;
     [SerializeField] private float _timeBeforeActivating = 2f;
     [SerializeField] private float _timeBeforeStartingAgain;
     
@@ -34,6 +35,7 @@ public class Bouncer : AbstractEnemy, iMovable
             _timer += Time.deltaTime;
             if (_timer > _timeBeforeActivating)
             {
+                //if (_isMoving) return;
                 Move(transform.position, player.transform.position, _speed);
             }
         }
@@ -42,12 +44,6 @@ public class Bouncer : AbstractEnemy, iMovable
             float desiredScale = Mathf.Lerp(transform.localScale.y, _initialScale, _speed * Time.deltaTime);
             transform.localScale = new Vector3(_initialScale , desiredScale, _initialScale);
         }
-
-        if (!_isMoving && !_isCoroutineRunning)
-        {
-            _timer = 0f;
-        }
-
     }
 
     public void Shrink()
@@ -89,6 +85,22 @@ public class Bouncer : AbstractEnemy, iMovable
         yield return new WaitForSeconds(_timeBeforeStartingAgain);
         
         _isCoroutineRunning = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!_isMoving) return;
+
+        if (collision != null && collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRb = collision.gameObject.GetComponentInParent<Rigidbody>();
+            Debug.Log("isMoving " + _isMoving);
+            if (playerRb != null)
+            {
+                Debug.Log("RIgidBody found!");
+                playerRb.AddForce(transform.up * _pushStrength, ForceMode.Impulse);
+            }
+        }
     }
 
 }
